7347b2426aa39b1e423cbbf296f1858573bf13fc
Erdun E <eed950128@gmail.com>
2024-09-11 22:22:28 -0400

    Initial Recitation 2

---
+++ CS5010ProgrammingDesignParadigm/Reciation2/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    /*
+    This is the main method where you will run the program.
+    Also, note how this is not a Javadoc-style comment!
+    It only begins with one asterisk, making it a general
+    comment that will not be pinned to the documentation.
+     */
+
+    public static void main(String[] args){
+        //Inside this method, set up your calculator class.
+        //When it runs, it should be able to produce
+        //a computation based on the values passed through
+        //as parameters.
+    }
+}
---
+++ CS5010ProgrammingDesignParadigm/Reciation2/src/Operations/Divide.java
@@ -0,0 +1,11 @@
+package Operations;
+/**
+ * This class implements Computable, and will take two
+ * integers as parameters. It will return those integers
+ * as the division of them.
+ *
+ * Remember that your division class needs a try/catch block
+ * to handle a "divide by zero" exception.
+ */
+public class Divide {
+}
