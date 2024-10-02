aa7382684c4b9240762fd17e92df0fdafff69cf2
Erdun E <eed950128@gmail.com>
2024-10-02 19:35:21 -0400

    CS5010 Recitation 5 added CalculatorTest

---
+++ CS5010ProgrammingDesignParadigm/Recitation5/Recitation5/src/CalculatorTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CalculatorTest {
+
+    // Test calculator
+    @Test
+    public void testCalculatorAdd() {
+        assertEquals(10, Calculator.run('+', 9, 1), "9 + 1 added should equal to 10");
+    }
+
+    @Test
+    public void testCalculatorubtract() {
+        assertEquals(8,Calculator.run('-', 9, 1), "9 - 1 subtracted should equal to 8");
+    }
+
+    @Test
+    public void testCalculatorMultiply() {
+        assertEquals(9, Calculator.run('*', 9, 1), "9 * 1 multiplied should equal to 9");
+    }
+
+    @Test
+    public void testCalculatorDivide() {
+        assertEquals(9, Calculator.run('/', 9, 1), "9 / 1 divided should equal to 9");
+    }
+
+    @Test
+    public void testCalculatorDivideByZero() {
+        assertThrows(ArithmeticException.class, () -> {
+            Calculator.run('/', 9, 0);
+        }, "Division by zero should throw ArithmeticException");
+    }
+
+    @Test
+    public void testCalculatorInvalidOperation() {
+        assertEquals(0, Calculator.run('%',1,9),"Invalid operation should return 0 or default return");
+    }
+}
