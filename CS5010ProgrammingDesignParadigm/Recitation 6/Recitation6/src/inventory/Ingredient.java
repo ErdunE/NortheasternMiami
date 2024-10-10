c0673d54b1e2b50ea8b5bd621473d2d3c3480b3f
Erdun E <eed950128@gmail.com>
2024-10-10 01:06:25 -0400

    CS5010 Recitation 6 Ingredient.java Added Metrics Analysis

--- CS5010ProgrammingDesignParadigm/Recitation 6/Recitation6/src/inventory/Ingredient.java
+++ CS5010ProgrammingDesignParadigm/Recitation 6/Recitation6/src/inventory/Ingredient.java
@@ -2,17 +2,19 @@ package inventory;
 
 /**
  * WMC
- *
+ * 4, getName, getQuantity, setQuantity, constructor
  * DIT
- *
+ * 1
  * NOC
- *
+ * 0
  * CBO
- *
+ * 0
  * RFC
- *
+ * 4
+ * 4 internal, getName, getQuantity, setQuantity, constructor
  * LCOM
- *
+ * Low, all methods operate on ingredientName and quantity.
+ * So they are highly cohesive.
  */
 
 public class Ingredient {
