aa370bd0fcb9bf18bccea35c0d0520be1f14d5cc
Erdun E <eed950128@gmail.com>
2024-10-11 00:37:14 -0400

    CS5010 Mid-Semester Initial Movie.java

---
+++ CS5010ProgrammingDesignParadigm/Mid-Sememter Assignment/Mid-Sememter Assignment/src/model/Movie.java
@@ -0,0 +1,8 @@
+package model;
+
+// Planed to complete for final project
+// Simple model class representing a movie
+// Will evolve to include more details (e.g., year, director).
+
+public class Movie {
+}
