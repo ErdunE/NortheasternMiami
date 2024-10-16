68b5bb6847b666562ca0d281c0c5a5e1c77c5ca2
Erdun E <eed950128@gmail.com>
2024-10-16 17:21:49 -0400

    CS5010 Assignment 2 ProductionSystem.java Implemented most method and added Javadoc Comment

--- CS5010ProgrammingDesignParadigm/Assignment 2 Solving the Brewery Problem/Brewery Control System/src/production/ProductionSystem.java
+++ CS5010ProgrammingDesignParadigm/Assignment 2 Solving the Brewery Problem/Brewery Control System/src/production/ProductionSystem.java
@@ -1,35 +1,82 @@
 package production;
 
+import java.util.ArrayList;
+import java.util.List;
 import inventory.InventorySystem;
 import recipe.RecipeLibrary;
 
 /**
- * WMC
- * 2, constructor, getProductionController
- * DIT
- * 1
- * NOC
- * 0
- * CBO
- * 1 productionController
- * RFC
- * 3
- * 2 internal, constructor, getProductionController
- * 1 external, getProductionController
- * LCOM
- * Low, all methods focus on managing the production system
+ * The ProductionSystem class manages the entire production process in the
+ * Brewery Control System. It handles the creation and monitor of batches,
+ * as well as interactions with the vats and transfers between them. The
+ * production system also integrates with the inventory system to manage
+ * ingredient stock and the recipe library for creating batches.
+
+ *
+ * @author Erdun
+ * @version 1.3
+ * @since 10/15/2024
  */
-
 public class ProductionSystem {
-
     private ProductionController productionController;
+    private List<Batch> batches;
 
+    /**
+     * Constructor for the ProductionSystem class.
+     *
+     * @param inventorySystem The inventory system to manage ingredient stocks.
+     * @param recipeLibrary The recipe library to retrieve beverage recipes.
+     */
     public ProductionSystem(InventorySystem inventorySystem, RecipeLibrary recipeLibrary) {
-        this.productionController = new ProductionController(inventorySystem, recipeLibrary);
+        this.productionController = new ProductionController(inventorySystem, recipeLibrary, this);
+        // Initializes the list to hold all production batches
+        this.batches = new ArrayList<>();
     }
 
+    /**
+     * Returns the production controller responsible for managing production tasks.
+     *
+     * @return The ProductionController for handling operations like create and manage batch.
+     */
     public ProductionController getProductionController() {
         return productionController;
     }
 
+    /**
+     * Returns the list of all batches currently in production.
+     *
+     * @return A list of Batch represent the batches in production.
+     */
+    public List<Batch> getBatches() {
+        return batches;
+    }
+
+    /**
+     * Add a new batch to the production system.
+     *
+     * @param batch The Batch to be added to the production system.
+     */
+    public void addBatch(Batch batch) {
+        // Adds the batch to the internal list of batches
+        batches.add(batch);
+    }
+
+    /**
+     * Finds and returns a batch based on the given batch ID.
+     *
+     * @param batchId The ID of the batch to be retrieved.
+     * @return The Batch with the matching batch ID, or null if not found.
+     */
+    public Batch findBatchById(int batchId) {
+        // Loop through each batch in the list to find the batch with the given ID
+        for (Batch batch : batches) {
+            // Check if the batch ID matches the input ID
+            if (batch.getBatchID() == batchId) {
+                // Return the batch if found
+                return batch;
+            }
+        }
+        // Return null if no batch with the specified ID is found
+        return null;
+    }
 }
