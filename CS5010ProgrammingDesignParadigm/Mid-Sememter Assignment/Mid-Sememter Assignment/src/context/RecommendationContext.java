e9fafbdcecedce93302f5a6cbc71a1c8c8fe725b
Erdun E <eed950128@gmail.com>
2024-10-18 11:09:06 -0400

    Version 1.1 Optimized javadoc comments

--- CS5010ProgrammingDesignParadigm/Mid-Sememter Assignment/Mid-Sememter Assignment/src/context/RecommendationContext.java
+++ CS5010ProgrammingDesignParadigm/Mid-Sememter Assignment/Mid-Sememter Assignment/src/context/RecommendationContext.java
@@ -4,14 +4,14 @@ import strategy.RecommendationStrategy;
 import java.util.List;
 
 /**
- * @author Erdun E
- * @version 1.0
- * @since 2024-10-01
- * Course: CS5010 Program Design Paradigm
- * Program: Mid-Semester Assignment
- *
  * Context class to manage different recommendation strategies.
  * Allows switching between strategies dynamically.
+ *
+ * @author Erdun E
+ * @version 1.1
+ * @since 2024-10-18
+ * Course: CS5010 Program Design Paradigm
+ * Program: Mid-Semester Assignment
  */
 
 public class RecommendationContext {
@@ -20,6 +20,7 @@ public class RecommendationContext {
 
     /**
      * Set the current recommendation strategy.
+     *
      * @param strategy The strategy to be applied.
      */
     public void setRecommendationStrategy(RecommendationStrategy strategy) {
@@ -28,6 +29,7 @@ public class RecommendationContext {
 
     /**
      * Get recommendations from the current strategy.
+     *
      * @return List of recommended media titles.
      */
     public List<String> getRecommendations(){
