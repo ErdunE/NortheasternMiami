9badaccc420461ff21ca2bb75eff6a6d274f6644
Erdun E <eed950128@gmail.com>
2024-10-18 11:09:14 -0400

    Version 1.1 Optimized javadoc comments

--- CS5010ProgrammingDesignParadigm/Mid-Sememter Assignment/Mid-Sememter Assignment/src/strategy/PopularRecommendation.java
+++ CS5010ProgrammingDesignParadigm/Mid-Sememter Assignment/Mid-Sememter Assignment/src/strategy/PopularRecommendation.java
@@ -4,17 +4,22 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
+ * PopularRecommendation class provides popular movie recommendations.
+ *
  * @author Erdun E
- * @version 1.0
- * @since 2024-10-01
+ * @version 1.1
+ * @since 2024-10-18
  * Course: CS5010 Program Design Paradigm
  * Program: Mid-Semester Assignment
- *
- * PopularRecommendation class provides popular movie recommendations.
  */
 
 public class PopularRecommendation implements RecommendationStrategy {
 
+    /**
+     * Returns a list of popular movie recommendations.
+     *
+     * @return a List of popular movies examples
+     */
     @Override
     public List<String> getRecommendations() {
         return Arrays.asList("Inception", "The Dark Knight", "Interstellar");
