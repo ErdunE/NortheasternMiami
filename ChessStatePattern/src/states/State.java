e13b293f1d09aea021e6a8d71a9c40ef686c3b9e
xinyuwang <wangxx039@gmail.com>
2024-10-31 09:47:38 -0400

    init State

--- ChessStatePattern/src/states/State.java
+++ ChessStatePattern/src/states/State.java
@@ -1,16 +1,35 @@
 package states;
 
 import main.*;
+import states.commons.Move;
+
+import java.util.List;
 
 public abstract class State {
 	
 	ChessGame game;
-	
+	String activePlayer;
+	List<Move> moveHistory;
+
 	public State(ChessGame game) {
 		this.game = game;
+		moveHistory = new ArrayList<>();
 	}
-	
-	//here is where you write the methods you will want overridden by each
-	//specific state's behavior
+
+	public void setActivePlayer(String playerName) {
+		this.activePlayer = playerName;
+	}
+
+	public String getActivePlayer() {
+		return this.activePlayer;
+	}
+
+	public void addMove(Move move) {
+		moveHistory.add(move);
+	}
+
+	public List<Move> getMoveHistory() {
+		return moveHistory;
+	}
 	
 }
---
+++ ChessStatePattern/src/states/commons/Move.java
@@ -0,0 +1,26 @@
+package states.commons;
+
+public class Move {
+    private int row;
+    private int col;
+    public Move(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+}
