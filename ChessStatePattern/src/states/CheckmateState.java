ec9d2fa8405b0561147dcb20a8834eca82e7e98e
Erdun E <eed950128@gmail.com>
2024-11-01 00:08:19 -0400

    Update CheckmateState with placeholder methods

--- ChessStatePattern/src/states/CheckmateState.java
+++ ChessStatePattern/src/states/CheckmateState.java
@@ -1,4 +1,23 @@
 package states;
 
-public class CheckMate {
+import main.ChessGame;
+
+/**
+ * The {@code CheckmateState} class represents the end state of the game when
+ * the King is in checkmate. This state means that the game is over and
+ * one of the players has won.
+ *
+ *  @author Erdun E
+ *  @version 1.0
+ *  @since 2024-10-31
+ */
+public class CheckmateState extends State {
+    public CheckmateState(ChessGame chessGame) {
+        super(chessGame);
+    }
+
+    // Placeholder for actions that will be taken in the checkmate state.
+
+    // Placeholder method indicating the game is over in the checkmate state.
 }
+
