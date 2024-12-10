e2fe6ad834e87abd23d3e84b598b36853e6a7e46
Erdun E <eed950128@gmail.com>
2024-12-10 17:59:16 -0500

    Version 26, Tue, Added Log function.

--- CS5010ProgrammingDesignParadigm/Final Project/FinalProject/src/tests/FactoryTest.java
+++ CS5010ProgrammingDesignParadigm/Final Project/FinalProject/src/tests/FactoryTest.java
@@ -14,31 +14,4 @@ import static org.junit.jupiter.api.Assertions.*;
  * Unit tests for the RecommendationFactory class.
  */
 public class FactoryTest {
-
-    @Test
-    public void testCreatePopularStrategy() {
-        RecommendationStrategy strategy = RecommendationFactory.createStrategy("popular", null);
-        assertNotNull(strategy);
-        assertTrue(strategy instanceof PopularRecommendation);
-    }
-
-    @Test
-    public void testCreateGenreStrategy() {
-        RecommendationStrategy strategy = RecommendationFactory.createStrategy("genre", "Action");
-        assertNotNull(strategy);
-        assertTrue(strategy instanceof GenreRecommendation);
-    }
-
-    @Test
-    public void testCreateRatingStrategy() {
-        RecommendationStrategy strategy = RecommendationFactory.createStrategy("rating", null);
-        assertNotNull(strategy);
-        assertTrue(strategy instanceof RatingBasedRecommendation);
-    }
-
-    @Test
-    public void testInvalidStrategyType() {
-        RecommendationStrategy strategy = RecommendationFactory.createStrategy("invalid", null);
-        assertNull(strategy);
-    }
 }
