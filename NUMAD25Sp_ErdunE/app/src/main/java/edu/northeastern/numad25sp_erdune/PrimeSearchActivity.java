0c888632ded201e1c9b926cabb21e0e9217168db
Erdun E <eed950128@gmail.com>
2025-02-06 22:45:15 -0500

    Implemented prime directive button and new PrimeSearchActivity

---
+++ NUMAD25Sp_ErdunE/app/src/main/java/edu/northeastern/numad25sp_erdune/PrimeSearchActivity.java
@@ -0,0 +1,12 @@
+package edu.northeastern.numad25sp_erdune;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PrimeSearchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prime_search);
+    }
+}
