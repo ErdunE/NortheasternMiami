d7697246f120e17a6edf8e3a9323567f240e1f6b
Erdun E <eed950128@gmail.com>
2025-02-11 18:29:57 -0500

    Updated MainActivity layout with constraints and added placeholders for A6 & A7

--- app/src/main/java/edu/northeastern/a6_group_1/MainActivity.java
+++ app/src/main/java/edu/northeastern/a6_group_1/MainActivity.java
@@ -1,24 +1,37 @@
 package edu.northeastern.a6_group_1;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnPetFinder = findViewById(R.id.btn_pet_finder);
+        Button btnFirebase = findViewById(R.id.btn_firebase);
+
+        // Per Finder Button
+        btnPetFinder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PetFinderActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Firebase Button
+        btnFirebase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FirebaseActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
