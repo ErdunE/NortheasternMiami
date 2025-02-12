2cf592fa63ece601e447a1d5021b8b9215f2700e
Alp Yalcinkaya <yalcinkayaalp@gmail.com>
2025-02-12 14:51:25 -0500

    Implement API authentication for Petfinder

--- app/src/main/java/edu/northeastern/a6_group_1/PetFinderActivity.java
+++ app/src/main/java/edu/northeastern/a6_group_1/PetFinderActivity.java
@@ -1,12 +1,26 @@
 package edu.northeastern.a6_group_1;
 
 import android.os.Bundle;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PetFinderActivity extends AppCompatActivity {
+    private static final String TAG = "PetFinderActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_finder);
+
+        // Run network request in a background thread
+        new Thread(() -> {
+            String token = PetfinderAuth.getAccessToken();
+            if (token != null) {
+                Log.d(TAG, "Received Access Token: " + token);
+                // TODO: Use this token for fetching pet data
+            } else {
+                Log.e(TAG, "Failed to retrieve access token");
+            }
+        }).start();
     }
 }
