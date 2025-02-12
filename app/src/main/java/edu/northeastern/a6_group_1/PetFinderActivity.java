46a4eff89a10e033a760e70249883a60c51fd4b8
Jenny Nguyen <>
2025-02-11 23:30:49 -0800

    created the layout for home page and added a search bar for user's input

--- app/src/main/java/edu/northeastern/a6_group_1/PetFinderActivity.java
+++ app/src/main/java/edu/northeastern/a6_group_1/PetFinderActivity.java
@@ -1,12 +1,19 @@
 package edu.northeastern.a6_group_1;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PetFinderActivity extends AppCompatActivity {
+
+    private EditText searchBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_finder);
+
+        //Search bar for users to enter what they are looking for
+        searchBar = findViewById(R.id.searchBar);
     }
 }
